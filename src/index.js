1b6ed3c5cbb72548c2d8582ed9ef6d37c1c96779
Emma Chatley <emma@Emmas-MacBook-Air.local>
2018-10-16 01:09:04 +0100

    CSS styling updates.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import SpeechRecognition from 'react-speech-recognition'
 import './index.css';
